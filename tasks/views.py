93c0db51fdd85e27c56a63b33434f91366360f3c
Nurlan <nur23kg@mail.ru>
2023-02-02 05:14:28 +0600

    Homework model

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from tasks.models import Task
+from django.views.generic.list import ListView
+from students.models import Student
 
-# Create your views here.
+def index(request):
+    user = Student.objects.create(username='py_student_3', password='A123456b', band_id=2)
+    return render(request, user)
+    # return render(request, 'tasks/tasks.html', {'Tasks': Task.objects.filter(is_active=True).order_by('number')})
+
+
+
+# class TasksListView(ListView):
+#     model = Task
